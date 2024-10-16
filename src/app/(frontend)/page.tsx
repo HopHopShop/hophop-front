a65d68f86fa8de5667cac9a42636eb873bdea172
alex <alex.sanders@ukr.net>
2024-10-16 13:02:00 +0300

    Test pipeline

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,3 +1,4 @@
+import SectionContainer from '@/components/SectionContainer/SectionContainer';
 import Hero from '@/template/Hero/Hero';
 import NewArrivals from '@/template/NewArrivals/NewArrivals';
 import TopSales from '@/template/TopSales/TopSales';
@@ -7,6 +8,9 @@ export default function Home() {
 	return (
 		<>
 			<Hero />
+			<SectionContainer>
+				<div>TEST PIPELINE</div>
+			</SectionContainer>
 			<NewArrivals />
 			<TopSales />
 			<CategoryGridSection />
